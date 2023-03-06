cd2dc49dd1dd0c983bab13113ee7c60c68122ec5
Дмитрий <xkof3st.mail@gmail.com>
2023-03-06 15:04:02 +0300

    Добавлена анонимная авторизация

--- lib/main.dart
+++ lib/main.dart
@@ -2,17 +2,20 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_firebase_auth/firebase_options.dart';
 import 'package:flutter_firebase_auth/screens/email_password_auth_screen.dart';
+import 'package:flutter_firebase_auth/screens/guest_auth_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
+
+  final GlobalKey<NavigatorState> _navigator = GlobalKey<NavigatorState>();
 
   @override
   Widget build(BuildContext context) {
@@ -22,8 +25,45 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
+      navigatorKey: _navigator,
+      builder: (ctx, navigator) {
+        return Overlay(
+          initialEntries: [
+            OverlayEntry(
+              builder: (c) {
+                return SafeArea(child: navigator!);
+              },
+            ),
+            OverlayEntry(
+              builder: (c) {
+                return Row(
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {
+                        _navigator.currentState!.pushReplacementNamed(
+                          "/auth/email-password",
+                        );
+                      },
+                      child: const Text('Email + password'),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        _navigator.currentState!.pushReplacementNamed(
+                          "/auth/guest",
+                        );
+                      },
+                      child: const Text('As guest'),
+                    ),
+                  ],
+                );
+              },
+            ),
+          ],
+        );
+      },
       routes: {
-        "/auth/email-password": (ctx) => EmailPasswordAuthScreen()
+        "/auth/email-password": (ctx) => EmailPasswordAuthScreen(),
+        "/auth/guest": (ctx) => GuestAuthScreen(),
       },
       initialRoute: "/auth/email-password",
     );
